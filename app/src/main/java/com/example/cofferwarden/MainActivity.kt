e4922bf59b7fa7873d803e868c467aa5a2042d6e
GatariGSPDN4 <gatarigspdn@mail.ru>
2021-11-19 23:17:27 +0300

    created framework structure of app

--- app/src/main/java/com/example/cofferwarden/MainActivity.kt
+++ app/src/main/java/com/example/cofferwarden/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.cofferwarden
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cofferwarden.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //fuck
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = binding.fragmentContainerView
     }
 }
